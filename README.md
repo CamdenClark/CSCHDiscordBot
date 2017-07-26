d86efaccb5c42c8e06985b6fbce9d7c60129b9b2
Camden Clark <camdenclark2012@gmail.com>
2017-07-26 01:54:06 -0700

    First commit with working resume queue. Check readme for install info.
    Landmark: Allows users to add and take from resume queue. Has some basic
    checks to prevent certain types of abuse. We'll add more if it gets
    abused. Before any destructuring of code.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ResumeDiscordBot
 A discord bot to facilitate resume reviewing on the Twitch Plays Programming Discord server
+
+# Installation for development
+
+1. Clone this repository.
+2. Create a .env file similar to .env.example that has your bot token in it.
+3. Run ```npm install```.
+4. Run ```npm start```.
+
+# Roadmap
+
+1. Destructure this project well.
+2. Set up a system for controlling interviews.
---
+++ index.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const dotenv  = require("dotenv");
+const client  = new Discord.Client();
+
+var queue = [];
+
+dotenv.config();
+
+/* Yes, I know this is a mess. I'm just trying to get it functional, then
+ * we can be more discerning about the quality of this code and how to
+ * destructure it properly.
+ **/
+
+function handleResume(msg) {
+    function sendHelp(msg) {
+        msg.reply('!resume is the resume queue for this server.\n' +
+          'Use !resume submit [url to resume] to add a resume.\n' +
+          'Use !resume get to get a resume to review.\n' +
+          'Remember to mention the user so they see the comments you made!')
+    }
+    
+    function verifyAdded(msg) {
+        msg.reply(`successfully added you to the resume queue.`);
+    }
+
+    if (msg.channel.name === "bot-development") {
+        if (msg.content.startsWith('!resume')) {
+            const splitmsg = msg.content.split(" ");
+            console.log(splitmsg);
+            if ((splitmsg.length == 1) || (splitmsg[1] == 'help')) {
+                sendHelp(msg);
+            } else if ((splitmsg[1] == 'submit' || splitmsg[1] == 'add') && (splitmsg.length == 3)) {
+                if (queue.filter((auth) => auth[0].id == msg.author.id).length != 0) {
+                  msg.reply(`sorry, you already have a resume in the queue.`)
+                } else {
+                  queue.push([msg.author, splitmsg[2]]);
+                  verifyAdded(msg);
+                }
+            } else if (splitmsg[1] == 'get') {
+                if (queue.length == 0) {
+                    msg.reply("there are no resumes currently in the queue.");
+                } else {
+                    const reply = queue.shift();
+                    msg.reply(`resume by ${reply[0]}: ${reply[1]}`);
+                }
+            } else {
+              msg.reply("that's an invalid query. Try !resume help.");
+            }
+        }
+    }
+}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    handleResume(msg);
+});
+
+client.login(process.env.TOKEN_SECRET);
