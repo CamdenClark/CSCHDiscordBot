1b8f79b3837505c10fd820f1fda56ed4bde22603
Camden Clark <camdenclark2012@gmail.com>
2017-07-26 19:30:10 -0700

    Fixing switch statement stuff, bugfixes. "show" DMs now instead.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv  = require("dotenv");
 const client  = new Discord.Client();
 
 var queue = [];
+var backlog = [];
 
 dotenv.config();
 
@@ -16,8 +17,9 @@ function handleResume(msg) {
     function sendHelp(msg) {
         msg.reply('"!resume" is the resume queue for this server.\n' +
                   'Use "!resume submit <url to resume>" to add a resume.\n' +
-                  'Use "!resume poll" to get a resume to review.\n' +
-                  'Use "!resume show" to see the resumes currently in the queue.\n' +
+                  'Use "!resume poll" to get a resume to review and delete it from the queue.\n' +
+                  'Use "!resume show" to see the next 3 resumes currently in the queue.\n' +
+                  'Use "!resume delete" to delete a resume you submitted.\n' +
                   'Remember to mention the user so they see the comments you made!')
     }
 
@@ -25,54 +27,73 @@ function handleResume(msg) {
         msg.reply(`successfully added you to the resume queue.`);
     }
 
-    if (msg.channel.name === "resume-review") {
+    if (msg.channel.name === "resume-review" || msg.channel.name === "bot-development") {
         if (msg.content.toLowerCase().startsWith('!resume')) {
             const splitmsg = msg.content.split(" ");
             console.log(splitmsg);
-            switch(splitmsg[1].toLowerCase()) {
-                case 'help':
-                    if(splitmsg.length == 1) {//length verification
-                        sendHelp(msg);
-                    }
-                    break;
-                case 'submit':
-                case 'add':
-                    if(splitmsg.length == 3) {//length verification
-                        if (queue.filter((auth) => auth[0].id == msg.author.id).length != 0) {
-                            msg.reply(`sorry, you already have a resume in the queue.`)
-                        } else {
-                            queue.push([msg.author, splitmsg[2]]);
-                            verifyAdded(msg);
+            if (splitmsg.length < 2) {
+              sendHelp(msg);
+            } else {
+                switch(splitmsg[1].toLowerCase()) {
+                    case 'help':
+                        if(splitmsg.length === 2) {//length verification
+                            sendHelp(msg);
                         }
-                    }
-                    break;
-                case 'poll':
-                    if(splitmsg.length == 1) {//length verification
-                        if (queue.length == 0) {
-                            msg.reply("there are no resumes currently in the queue.");
-                        } else {
-                            const reply = queue.shift();
-                            msg.reply(`resume by ${reply[0]}: ${reply[1]}`);
-                        }
-                    }
-                    break;
-                case 'show':
-                    if(splitmsg.length == 1) {//length verification
-                        if (queue.length == 0) {
-                            msg.reply("there are no resumes currently in the queue.");
-                        } else {
-                            msg.reply("resumes currently in the queue:\n\n");
-                            for(var i = 0; i < queue.length; i++){
-                                msg.channel.send(`${queue[i][0]}: ${queue[i][1]}`);
+                        break;
+                    case 'submit':
+                    case 'add':
+                        if(splitmsg.length === 3) {//length verification
+                            if (queue.filter((auth) => auth[0].id == msg.author.id).length != 0) {
+                                msg.reply(`sorry, you already have a resume in the queue.`)
+                            } else {
+                                queue.push([msg.author, '<' + splitmsg[2] + '>', []]);
+                                verifyAdded(msg);
                             }
                         }
-                    }
-                    break;
-                default:
-                    msg.reply("that's an invalid query. Try !resume help.");
-                    break;
+                        break;
+                    case 'poll':
+                        if(splitmsg.length === 2) {//length verification
+                            if (queue.length === 0) {
+                                msg.reply("there are no resumes currently in the queue.");
+                            } else {
+                                const reply = queue.shift();
+                                msg.reply(`resume by ${reply[0]}: ${reply[1]}`);
+                            }
+                        }
+                        break;
+                    case 'show':
+                        if(splitmsg.length === 2) {//length verification
+                            if (queue.length === 0) {
+                                msg.reply("there are no resumes currently in the queue.");
+                            } else {
+                                msg.author.createDM().then((dmChan) => {
+                                    dmChan.send("resumes currently in the queue:\n\n");
+                                    var showLength = queue.length < 3 ? queue.length : 3;
+                                    for(var i = 0; i < showLength; i++){
+                                        dmChan.send(`${queue[i][0]}: ${queue[i][1]}`);
+                                    }
+                                })
+                            }
+                        }
+                        break;
+                    case 'delete':
+                        if(splitmsg.length === 2) {
+                            if (queue.length === 0) {
+                                msg.reply("there are no resumes currently in the queue.");
+                            } else if (queue.filter((auth) => auth[0].id == msg.author.id).length == 0) {
+                                msg.reply("you don't have a resume in the queue.");
+                            } else {
+                                queue = queue.filter((auth) => auth[0].id != msg.author.id);
+                                msg.reply("successfully deleted your resume.");
+                            }
+                        }
+                        break;
+                    default:
+                        msg.reply("that's an invalid query. Try !resume help.");
+                        break;
+                }
             }
-        }
+        }           
     }
 }
 
