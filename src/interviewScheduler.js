bd5f9f4212875b6fc3135fa0c46659857d14c683
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-07-27 20:07:24 -0400

    hmmmm interviewScheduler

--- src/interviewScheduler.js
+++ src/interviewScheduler.js
@@ -1,5 +1,20 @@
 const { map, filter }  = require("lodash");
 
+/*
+not sure what I'm doing either
+ */
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/IVDB";
+
+MongoClient.connect(url, function(error, db) {
+    if(error) {
+        throw error;
+    }
+    db.createCollection("interviewees");
+
+});
+
+
 module.exports = function handleIVRequest(msg, prod) {
 
 }
