ab031ad3a67914007df3a9326c351761c8a0f6bb
CBSkarmory <CBSkarmory@users.noreply.github.com>
2017-07-29 15:06:20 -0400

    redundant ternary operator

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const handleResume = require("./resumeHandler.js");
 const client = new Discord.Client();
 
 dotenv.config();
-const productionEnv = process.env.ENVIRONMENT === "DEV" ? false : true;
+const productionEnv = !(process.env.ENVIRONMENT === "DEV");
 
 /* Yes, I know this is a mess. I'm just trying to get it functional, then
  * we can be more discerning about the quality of this code and how to
